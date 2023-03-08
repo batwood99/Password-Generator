c2ae9fbd27a0e08712a65a5d2d61dd72f3a76f73
batwood99 <atwood@mail.usf.edu>
2023-03-08 13:45:53 -0500

    added prompt that asks how long password should be. Also added prompt if user enters invalid number

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    var passwordlength = prompt("How long would you like your password to be? (Enter value between 8 and 128)");
+    while (passwordlength < 8 || passwordlength > 128) {
+        passwordlength = prompt("Invalid. Enter a value between 8 and 128.");
+    }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
